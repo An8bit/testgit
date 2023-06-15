ccce0a1844af9ad85c58c4993759d84da21d7803
An8bit <ttkit35@gmail.com>
2023-06-15 22:20:50 +0700

    'hi'

---
+++ BT1.py
@@ -0,0 +1,12 @@
+def tim_uoc_chung_lon_nhat(a, b):
+    if a > b:
+        lon, nho = a, b
+    else:
+        lon, nho = b, a
+    while nho != 0:
+        temp = nho
+        nho = lon % nho
+        lon = temp
+    return print("manh nhat",lon)
+    
+tim_uoc_chung_lon_nhat(24,6)
