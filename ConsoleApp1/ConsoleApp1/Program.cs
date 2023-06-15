b2fabcab249f202c675d8f9ce1a7b4359708800d
anhtrungju <73046556+anhtrungju@users.noreply.github.com>
2023-06-15 23:03:04 +0700

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //thành an aaaaaaaaaaaaadsssssssbsakfhksabfsahhhhhhhhhh
+            //hehehe
             
         }
     }
